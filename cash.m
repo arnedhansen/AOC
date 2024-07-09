dbe58286397ddce3882a7cc39504b28e2313e2c9
Arne <arne96.hansen@gmail.com>
2024-07-09 13:33:30 +0200

    paradigms setup

--- cash.m
+++ cash.m
@@ -17,7 +17,7 @@ subjectID = str2num(subjectID);
 
 %% Load data file and extract cash amount
 
-load([DATA_PATH, '/', num2str(subjectID), '/', num2str(subjectID), '_OCC_NBack_block3_task.mat']);
+load([DATA_PATH, '/', num2str(subjectID), '/', num2str(subjectID), '_OCC_NBack_block6_task.mat']);
 cashNback = saves.amountCHFextraTotal;
 
 load([DATA_PATH, '/', num2str(subjectID), '/', num2str(subjectID), '_OCC_Sternberg_block8_task.mat']);

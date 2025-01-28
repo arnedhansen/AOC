826fb0cfc6f848bc5cf940384bd2f7b9a3943978
Arne Hansen <arne96.hansen@gmail.com>
2025-01-28 10:55:57 +0100

    fix dialogID

--- paradigms/dialogID.m
+++ paradigms/dialogID.m
@@ -2,21 +2,21 @@
 defAns = {'999','XX','99','X', 'X', 'X', '99'};
 while true
     prompt = {'Subject Number', 'Subject Initials', 'Subject Age', 'Subject Gender', 'EEG Cap Size', 'Ocular Dominance', 'Time Hair Washing'};
-    box = inputdlg(prompt, 'Enter Subject Information', 1,defAns);
-    subject.ID = char(box(1));
-    subject.initials = char(box(2));
-    subject.age = char(box(3));
-    subject.gender = char(box(4));
-    subject.capsize = char(box(5));
-    subject.ocDom = char(box(6));
-    subject.hairwash = char(box(7));
-    if length(subject.ID) == 3 && length(subject.initials) == 2               % Ensure response made in subject ID
+    box = inputdlg(prompt, 'Enter Subject Information', 1, defAns);
+    subject.ID = str2double(box{1}); % Convert directly to a number
+    subject.initials = char(box{2});
+    subject.age = str2double(box{3}); % Convert directly to a number
+    subject.gender = char(box{4});
+    subject.capsize = char(box{5});
+    subject.ocDom = char(box{6});
+    subject.hairwash = str2double(box{7}); % Convert directly to a number
+    % Ensure that Subject ID and initials are valid
+    if ~isnan(subject.ID) && length(box{1}) == 3 && length(subject.initials) == 2
         break
+    else
+        errordlg('Invalid Subject ID or Initials. Please try again.');
     end
 end
 
-% Convert subject data to numeric
-subject.ID = str2num(subject.ID);
+% Assign subjectID for further use
 subjectID = subject.ID;
-subject.age = str2num(subject.age);
-subject.hairwash = str2num(subject.hairwash);

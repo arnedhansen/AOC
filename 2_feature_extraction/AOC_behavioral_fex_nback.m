44eba658484de71279cba8553d83fb7562109ef1
Arne Hansen <arne96.hansen@gmail.com>
2025-03-04 14:30:55 +0100

    aoc nback behav fex missing data fix

--- 2_feature_extraction/AOC_behavioral_fex_nback.m
+++ 2_feature_extraction/AOC_behavioral_fex_nback.m
@@ -1,5 +1,5 @@
 %% AOC Behavioral Feature Extraction N-back
-% 
+%
 % Extracted features:
 %   Accuracy
 %   Reaction Time
@@ -18,7 +18,7 @@ behav_data_nback = struct('ID', {}, 'Condition', {}, 'Accuracy', {}, 'ReactionTi
 for subj = 1:length(subjects)
     datapath = strcat(path, subjects{subj});
     cd(datapath)
-    
+
     % Initialize subject-specific arrays
     subject_id = [];
     trial_num = [];
@@ -28,34 +28,49 @@ for subj = 1:length(subjects)
     stimuli = [];
     probe = [];
     match = [];
-    
+
     %% Read blocks
     trial_counter = 1;
     for block = 1:6
         fileNback = dir(strcat(subjects{subj}, '_AOC_Nback_block', num2str(block), '_*_task.mat'));
-        load(fileNback.name)
-        num_trials = length(saves.data.correct);
+        try
+            load(fileNback.name)
+            num_trials = length(saves.data.correct);
 
-        % Extract condition from file name
-        tokens = regexp(fileNback.name, '_\d+back_', 'match');
-        token = tokens{1}; % There should be only one match
-        cond = str2double(token(2)); % Convert the number to double
-        
-        % Append data for this block
-        subject_id = [subject_id; repmat({saves.subject.ID}, num_trials, 1)];
-        trial_num = [trial_num; (trial_counter:(trial_counter + num_trials - 1))'];
-        condition = [condition; repmat({cond}, num_trials, 1)];
-        accuracy = [accuracy; saves.data.correct'];
-        reaction_time = [reaction_time; saves.data.reactionTime'];
-        stimuli = [stimuli; saves.data.stimulus'];
-        match = [match; saves.data.match'];
+            % Extract condition from file name
+            tokens = regexp(fileNback.name, '_\d+back_', 'match');
+            token = tokens{1}; % There should be only one match
+            cond = str2double(token(2)); % Convert the number to double
+
+            % Append data for this block
+            subject_id = [subject_id; repmat({saves.subject.ID}, num_trials, 1)];
+            trial_num = [trial_num; (trial_counter:(trial_counter + num_trials - 1))'];
+            condition = [condition; repmat({cond}, num_trials, 1)];
+            accuracy = [accuracy; saves.data.correct'];
+            reaction_time = [reaction_time; saves.data.reactionTime'];
+            stimuli = [stimuli; saves.data.stimulus'];
+            match = [match; saves.data.match'];
+        catch ME
+            ME.message
+            disp(['ERROR loading Block ' num2str(block) '!'])
+            num_trials = 75;
+        end
         trial_counter = trial_counter + num_trials;
+    end
+    if isempty(subject_id)
+        subject_id = [subject_id; repmat({str2num(subjects{subj})}, num_trials, 1)];
+        trial_num = nan(75, 1);
+        condition = nan(75, 1);
+        accuracy = nan(75, 1);
+        reaction_time = nan(75, 1);
+        stimuli = nan(75, 1);
+        match = nan(75, 1);
     end
     % Convert ASCII numbers to letters
     stimuli = char(stimuli);
     % Set RT > 2 to NaN
     reaction_time(reaction_time > 2) = NaN;
-    
+
     %% Create a structure array for this subject
     subj_data_behav_trial = struct('ID', subject_id, 'Trial', num2cell(trial_num), 'Condition', condition, ...
         'Accuracy', num2cell(accuracy), 'ReactionTime', num2cell(reaction_time), 'Stimuli', num2cell(stimuli), 'Match', num2cell(match));
@@ -85,7 +100,7 @@ for subj = 1:length(subjects)
     save rt_nback l1rt l2rt l3rt
     clc
     disp(['Subject ' num2str(subj) '/' num2str(length(subjects)) ' done.'])
-    
+
     % Append to the final structure array
     behav_data_nback = [behav_data_nback; subj_data_behav];
 end

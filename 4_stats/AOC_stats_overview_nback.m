6c790599de98e1e7715d496b08e6483d39bff230
Arne Hansen <arne96.hansen@gmail.com>
2025-03-05 17:29:54 +0100

    Update AOC_stats_overview_nback.m

--- 4_stats/AOC_stats_overview_nback.m
+++ 4_stats/AOC_stats_overview_nback.m
@@ -1,4 +1,4 @@
-%% GCP Stats Overview
+%% AOC Stats Overview
 % Boxplots for all variables
 % Percentage change barplots
 % Alpha Differences vs. Gaze Differences
@@ -10,16 +10,12 @@ close all
 data = readtable('/Volumes/methlab/Students/Arne/AOC/data/features/merged_data_nback.csv');
 data.ReactionTime = data.ReactionTime .* 1000;
 data.PupilSize = [];
-variables = {'Accuracy', 'ReactionTime', 'GazeDeviation', 'GazeSTD', 'MSRate', 'Blinks', 'Fixations', 'Saccades', 'GammaPower', 'GammaFreq'};
-save_names = {'acc', 'rt', 'gazedev', 'gazestd', 'ms', 'blink', 'fix', 'sacc', 'pow', 'freq'};
-colors = color_def('GCP');
-
-% Split the data by contrast condition
-low_contrast = data(data.Condition == 1, :);
-high_contrast = data(data.Condition == 2, :);
+variables = {'Accuracy', 'ReactionTime', 'GazeDeviation', 'MSRate', 'Blinks', 'Fixations', 'Saccades', 'AlphaPower', 'IAF'};
+save_names = {'acc', 'rt', 'gazedev', 'ms', 'blink', 'fix', 'sacc', 'pow', 'iaf'};
+colors = color_def('AOC');
 
 %% BOXPLOTS for each variable
-y_axis_labels = {'Accuracy [%]', 'Reaction Time [ms]', 'Gaze Deviation [px]', 'Gaze STD [px]', 'Microsaccade Rate [ms/s]', 'Blinks', 'Fixations', 'Saccades', 'Gamma Power [dB]', 'Gamma Frequency [Hz]'};
+y_axis_labels = {'Accuracy [%]', 'Reaction Time [ms]', 'Gaze Deviation [px]', 'Gaze STD [px]', 'Microsaccade Rate [ms/s]', 'Blinks', 'Fixations', 'Saccades', 'Alpha Power [dB]', 'IAF [Hz]'};
 
 % Unique subject identifiers
 subjects = unique(data.ID);
@@ -36,12 +32,12 @@ for i = 1:length(variables)
     xlim([0.5 2.5])
 
     % Create boxplot 
-    boxplot(data.(variables{i}), data.Condition, 'Labels', {'Low Contrast', 'High Contrast'}, 'Colors', 'k');
+    boxplot(data.(variables{i}), data.Condition, 'Labels', {'1-back', '2-back', '3-back'}, 'Colors', 'k');
     set(gca, 'FontSize', 30); 
 
     % Define jitter
     jitterAmount = 0.1; % Adjust jitter if needed
-    for cond = 1:2
+    for cond = 1:3
         cond_data = data(data.Condition == cond, :);
         x_jittered{cond} = cond_data.Condition + (rand(size(cond_data.(variables{i}))) - 0.5) * jitterAmount;
     end
@@ -60,35 +56,34 @@ for i = 1:length(variables)
     end
 
     % Scatter individual data points
-    for cond = 1:2 % Two conditions (Low Contrast = 1, High Contrast = 2)
+    for cond = 1:3 
         cond_data = data(data.Condition == cond, :);
         scatter(x_jittered{cond}, cond_data.(variables{i}), 300, 'MarkerEdgeColor', 'k', ...
             'MarkerFaceColor', colors(cond, :), 'jitter', 'off', 'SizeData', 300);
     end
     
-
     % Add title and labels
-    title(variables{i}, "FontSize", 40);
+    title(['N-back ', variables{i}], "FontSize", 40);
     ylabel(y_axis_labels{i}, "FontSize", 30);
     set(gca, 'Box', 'off');
     ax = gca;
     ax.FontSize = font_size;
 
     % Save individual subplot
-    saveas(gcf, strcat('/Volumes/methlab/Students/Arne/GCP/figures/stats/overview/GCP_stats_boxplots_', save_names{i}, '.png'));
+    saveas(gcf, strcat('/Volumes/methlab/Students/Arne/AOC/figures/stats/overview/AOC_stats_boxplots_', save_names{i}, '_nback.png'));
 end
 
 %% BOXPLOTS OVERVIEW
 close all
 figure;
 set(gcf, 'Position', [100, 200, 2000, 1200], 'Color', 'w');
-y_axis_labels = {'Accuracy [%]', 'Reaction Time [ms]', 'Gaze Deviation [px]', 'Gaze STD [px]', 'Microsaccade Rate [ms/s]', 'Blinks', 'Fixations', 'Saccades', 'Gamma Power [dB]', 'Gamma Frequency [Hz]'};
+y_axis_labels = {'Accuracy [%]', 'Reaction Time [ms]', 'Gaze Deviation [px]', 'Gaze STD [px]', 'Microsaccade Rate [ms/s]', 'Blinks', 'Fixations', 'Saccades', 'Alpha Power [dB]', 'IAF [Hz]'};
 
 % Unique subject identifiers
 subjects = unique(data.ID);
 
 for i = 1:length(variables)
-    subplot(4, 3, i);
+    subplot(3, 3, i);
     hold on;
 
     % Set axis limits
@@ -96,7 +91,7 @@ for i = 1:length(variables)
     xlim([0.5 2.5])
 
     % Create boxplot
-    boxplot(data.(variables{i}), data.Condition, 'Labels', {'Low Contrast', 'High Contrast'}, 'Colors', 'k');
+    boxplot(data.(variables{i}), data.Condition, 'Labels', {'1-back', '2-back', '3-back'}, 'Colors', 'k');
     set(gca, 'FontSize', 15); 
 
     % Overlay individual data points and connect them
@@ -117,7 +112,7 @@ for i = 1:length(variables)
 
     % Scatter individual data points with jitter
     jitterAmount = 0.001;  % Amount of jitter in the x-direction
-    for cond = 1:2  % Two conditions (Low Contrast = 1, High Contrast = 2)
+    for cond = 1:3  % Two conditions (Low Contrast = 1, High Contrast = 2)
         % Filter data based on condition
         cond_data = data(data.Condition == cond, :);
 
@@ -134,7 +129,7 @@ for i = 1:length(variables)
     ylabel(y_axis_labels{i}, "FontSize", 15);
     hold off;
 end
-saveas(gcf, '/Volumes/methlab/Students/Arne/GCP/figures/stats/overview/GCP_stats_overview_boxplots.png');
+saveas(gcf, '/Volumes/methlab/Students/Arne/AOC/figures/stats/overview/AOC_stats_overview_boxplots_nback.png');
 
 %% PERCENTAGE CHANGE BARPLOTS for each variable
 close all;
@@ -147,10 +142,10 @@ for i = 1:length(variables)
         % Extract data for this subject
         subj_data = data(data.ID == subjects(subj), :);
 
-        if height(subj_data) == 2 % Ensure the subject has data for both conditions
+        if height(subj_data) == 3 % Ensure the subject has data for all conditions
             % Low and high contrast values
             low_value = subj_data{subj_data.Condition == 1, variables{i}};
-            high_value = subj_data{subj_data.Condition == 2, variables{i}};
+            high_value = subj_data{subj_data.Condition == 3, variables{i}};
 
             % Calculate percentage change ((high - low) / low) * 100
             percent_change(subj, i) = ((high_value - low_value) / low_value) * 100;
@@ -179,12 +174,12 @@ for i = 1:length(variables)
     xticklabels(subjects);
     xlabel('Subjects', 'FontSize', 25); % Set font size for xlabel
     ylabel('% Change', 'FontSize', 25); % Set font size for ylabel
-    title(variables{i}, 'FontSize', 30);
+    title(['N-back ', variables{i}], 'FontSize', 30);
 
     hold off;
 
     % Save individual bar plot
-    saveas(gcf, strcat('/Volumes/methlab/Students/Arne/GCP/figures/stats/overview/GCP_stats_barplots_', save_names{i}, '.png'));
+    saveas(gcf, strcat('/Volumes/methlab/Students/Arne/AOC/figures/stats/overview/AOC_stats_barplots_', save_names{i}, '_nback.png'));
 
     % Close the individual figure to free memory
     close(gcf);
@@ -201,10 +196,10 @@ for i = 1:length(variables)
         % Extract data for this subject
         subj_data = data(data.ID == subjects(subj), :);
 
-        if height(subj_data) == 2 % Ensure the subject has data for both conditions
+        if height(subj_data) == 3 % Ensure the subject has data for all conditions
             % Low and high contrast values
             low_value = subj_data{subj_data.Condition == 1, variables{i}};
-            high_value = subj_data{subj_data.Condition == 2, variables{i}};
+            high_value = subj_data{subj_data.Condition == 3, variables{i}};
 
             % Calculate percentage change ((high - low) / low) * 100
             percent_change(subj, i) = ((high_value - low_value) / low_value) * 100;
@@ -219,7 +214,7 @@ figure;
 set(gcf, 'Position', [100, 200, 2000, 1200], 'Color', 'w');
 
 for i = 1:length(variables)
-    subplot(4, 3, i);
+    subplot(3, 3, i);
     hold on;
 
     % Bar plot for each participant
@@ -240,29 +235,24 @@ for i = 1:length(variables)
     hold off;
 end
 sgtitle('Percentage Change (HC - LC)', 'FontSize', 24);
-saveas(gcf, '/Volumes/methlab/Students/Arne/GCP/figures/stats/overview/GCP_stats_overview_barplots_percentage_change.png');
+saveas(gcf, '/Volumes/methlab/Students/Arne/AOC/figures/stats/overview/AOC_stats_overview_barplots_percentage_change_nback.png');
 
-%% Plot DIFFERENCES in GAMMA POWER and FREQUENCY against GAZE METRICS: MICROSACCADES,
-%  GAZE DEVIATION and labels by ET (BLINKS, FIXATIONS, SACCADES)
+%% Plot DIFFERENCES in ALPHA POWER against GAZE METRICS 
+% MICROSACCADES,GAZE DEVIATION and labels by ET (BLINKS, FIXATIONS, SACCADES)
 close all
-% Calculate the differences in Gamma Power and Gamma Frequency
-gamma_power_diff = percent_change(:, 9);
-gamma_freq_diff = percent_change(:, 10);
+%variables = {'Accuracy', 'ReactionTime', 'GazeDeviation', 'MSRate', 'Blinks', 'Fixations', 'Saccades', 'AlphaPower', 'IAF'};
+alpha_power_diff = percent_change(:, 8);
 
 % Extract the corresponding values for gaze metrics
 gaze_deviation_diff = percent_change(:, 3);
-gaze_std_diff = percent_change(:, 4);
-ms_rate_diff = percent_change(:, 5);
-blink_diff = percent_change(:, 6);
-fix_diff = percent_change(:, 7);
-sacc_diff = percent_change(:, 8);
+ms_rate_diff = percent_change(:, 4);
+fix_diff = percent_change(:, 6);
+sacc_diff = percent_change(:, 7);
 
 % Define gaze metrics
 metrics = {
     'Gaze Deviation Diff. [%]', gaze_deviation_diff;
-    'Gaze STD Diff. [%]', gaze_std_diff;
     'Microsaccade Rate Diff. [%]', ms_rate_diff;
-    'Blink Diff. [%]', blink_diff;
     'Fixation Diff. [%]', fix_diff;
     'Saccade Diff. [%]', sacc_diff
 };
@@ -270,86 +260,71 @@ metrics = {
 % Number of metrics
 n_metrics = size(metrics, 1);
 
-% Create figure for Gamma Power
+% Create figure for Alpha Power
 figure;
 set(gcf, 'Position', [0, 0, 800, 800], 'Color', 'w');
 for i = 1:n_metrics
     subplot(ceil(n_metrics / 2), 2, i);
     hold on;
-    scatter(metrics{i, 2}, gamma_power_diff, 36, 'MarkerEdgeColor', [0, 0, 0], 'MarkerFaceColor', [0, 0, 0]);
+    scatter(metrics{i, 2}, alpha_power_diff, 36, 'MarkerEdgeColor', [0, 0, 0], 'MarkerFaceColor', [0, 0, 0]);
     xlabel(metrics{i, 1}, 'FontSize', 15);
-    ylabel('Gamma Power Diff. [%]', 'FontSize', 15);
+    ylabel('Alpha Power Diff. [%]', 'FontSize', 15);
     xlim([-100, 100]);
     ylim([-100, 100]);
     xline(0, '--', 'Color', [0.3, 0.3, 0.3], 'LineWidth', 0.5, 'Alpha', 0.25);
     yline(0, '--', 'Color', [0.3, 0.3, 0.3], 'LineWidth', 0.5, 'Alpha', 0.25);
-    title(['Pow vs ', metrics{i, 1}], 'FontSize', 18);
+    title(['Alpha vs ', metrics{i, 1}], 'FontSize', 18);
+    %lsline()
     hold off;
 end
-saveas(gcf, '/Volumes/methlab/Students/Arne/GCP/figures/stats/overview/GCP_stats_overview_associations_pow.png');
-
-% Create figure for Gamma Frequency
-figure;
-set(gcf, 'Position', [800, 0, 800, 800], 'Color', 'w');
-for i = 1:n_metrics
-    subplot(ceil(n_metrics / 2), 2, i);
-    hold on;
-    scatter(metrics{i, 2}, gamma_freq_diff, 36, 'MarkerEdgeColor', [0, 0, 0], 'MarkerFaceColor', [0, 0, 0]);
-    xlabel(metrics{i, 1}, 'FontSize', 15);
-    ylabel('Gamma Frequency Diff. [%]', 'FontSize', 15);
-    xlim([-100, 100]);
-    ylim([-100, 100]);
-    xline(0, '--', 'Color', [0.3, 0.3, 0.3], 'LineWidth', 0.5, 'Alpha', 0.25);
-    yline(0, '--', 'Color', [0.3, 0.3, 0.3], 'LineWidth', 0.5, 'Alpha', 0.25);
-    title(['Freq vs ', metrics{i, 1}], 'FontSize', 18);
-    hold off;
-end
-saveas(gcf, '/Volumes/methlab/Students/Arne/GCP/figures/stats/overview/GCP_stats_overview_associations_freq.png');
+saveas(gcf, '/Volumes/methlab/Students/Arne/AOC/figures/stats/overview/AOC_stats_overview_associations_pow_nback.png');
 
 %% CORRELATION matrix
-% close all
-% % Calculate the correlation matrix between variables
-% corr_matrix = corr(table2array(data(:, variables)), 'Rows', 'pairwise');
-% 
-% % Correlation matrix
-% % Generate heatmap
-% figure('Color', 'white');
-% set(gcf, 'Position', [200, 400, 1200, 1500]);
-% h = heatmap(corr_matrix);
-% 
-% % Define and add color map
-% num_colors = 256;
-% cpoints = [0, 0, 0, 1;      % blue at the lowest value
-%            0.46, 1, 1, 1;  % transition to white starts
-%            0.54, 1, 1, 1;  % transition from white ends
-%            1, 1, 0, 0];    % red at the highest value
-% 
-% % Preallocate the colormap array.
-% cmap = zeros(num_colors, 3);
-% 
-% % Linearly interpolate to fill in the colormap.
-% for i=1:size(cmap,1)
-%     % Normalize the current index to the 0-1 range based on the colormap size.
-%     val = (i-1)/(num_colors-1);
-%     % Find the first point where the value is greater than the current normalized value.
-%     idx = find(cpoints(:,1) >= val, 1);
-%     if idx == 1
-%         % Use the first colour for values below the first point.
-%         cmap(i,:) = cpoints(1,2:4);
-%     else
-%         % Linearly interpolate between the two bounding colours.
-%         range = cpoints(idx,1) - cpoints(idx-1,1);
-%         frac = (val - cpoints(idx-1,1)) / range;
-%         cmap(i,:) = (1-frac)*cpoints(idx-1,2:4) + frac*cpoints(idx,2:4);
-%     end
-% end
-% colormap(h, cmap);
-% 
-% % Customization
-% h.Title = 'Correlation Heatmap';
-% h.XDisplayLabels = variables; % Assuming varNames contains variable names excluding 'Date', 'Weekday', and 'Overall Score'
-% h.YDisplayLabels = variables;
-% h.ColorLimits = [-1, 1]; % Color limits to ensure proper color mapping
-% h.FontSize = 12; % Increase the size of the x and y axis tick labels
-% hTitle.FontSize = 25;
-saveas(gcf, '/Volumes/methlab/Students/Arne/GCP/figures/stats/overview/GCP_stats_overview_correlation_matrix.png');
+close all
+% Calculate the correlation matrix between variables
+corr_matrix = corr(table2array(data(:, variables)), 'Rows', 'pairwise');
+
+% Correlation matrix
+% Generate heatmap
+figure('Color', 'white');
+set(gcf, 'Position', [200, 400, 1200, 1500]);
+h = heatmap(corr_matrix);
+
+% Define and add color map
+num_colors = 256;
+cpoints = [0, 0, 0, 1;      % blue at the lowest value
+           0.46, 1, 1, 1;  % transition to white starts
+           0.54, 1, 1, 1;  % transition from white ends
+           1, 1, 0, 0];    % red at the highest value
+
+% Preallocate the colormap array.
+cmap = zeros(num_colors, 3);
+
+% Linearly interpolate to fill in the colormap.
+for i=1:size(cmap,1)
+    % Normalize the current index to the 0-1 range based on the colormap size.
+    val = (i-1)/(num_colors-1);
+    % Find the first point where the value is greater than the current normalized value.
+    idx = find(cpoints(:,1) >= val, 1);
+    if idx == 1
+        % Use the first colour for values below the first point.
+        cmap(i,:) = cpoints(1,2:4);
+    else
+        % Linearly interpolate between the two bounding colours.
+        range = cpoints(idx,1) - cpoints(idx-1,1);
+        frac = (val - cpoints(idx-1,1)) / range;
+        cmap(i,:) = (1-frac)*cpoints(idx-1,2:4) + frac*cpoints(idx,2:4);
+    end
+end
+colormap(h, cmap);
+
+% Customization
+h.Title = 'Correlation Heatmap';
+h.XDisplayLabels = variables; % Assuming varNames contains variable names excluding 'Date', 'Weekday', and 'Overall Score'
+h.YDisplayLabels = variables;
+h.ColorLimits = [-1, 1]; % Color limits to ensure proper color mapping
+h.FontSize = 12; % Increase the size of the x and y axis tick labels
+hTitle.FontSize = 25;
+
+% Save
+saveas(gcf, '/Volumes/methlab/Students/Arne/AOC/figures/stats/overview/AOC_stats_overview_correlation_matrix_nback.png');

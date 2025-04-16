88345e1485c83e1faabbcb0dbbab64aebfa87b45
Arne Hansen <arne96.hansen@gmail.com>
2025-04-16 17:22:24 +0200

    topos baselined working

--- 3_visualization/eeg/topos/AOC_eeg_topos_sternberg_baseline.m
+++ 3_visualization/eeg/topos/AOC_eeg_topos_sternberg_baseline.m
@@ -38,8 +38,6 @@ gatfr6 = ft_freqgrandaverage([],tfr6_all{:});
 %% Plot alpha power TOPOS
 close all
 alpha_range = [8 14];
-timepoints = 0:0.25:2;
-timepnts = length(timepoints)-1;
 
 % Plot
 fig = figure('Color', 'w', 'Position', [100, 100, 2000, 1200]);
@@ -49,7 +47,7 @@ title('Sternberg WM load 6', 'FontSize', 25)
 cfg = [];
 cfg.layout = headmodel.layANThead;
 allchannels = cfg.layout.label;
-cfg.figure = ax;
+cfg.figure = fig;
 
 %cfg.figure = 'no';
 cfg.channel = allchannels(1:end-2);
@@ -65,28 +63,25 @@ cmap = flipud(cbrewer('div', 'RdBu', 64));
 cfg.colormap = cmap;
 cfg.gridscale = 300;
 cfg.ylim = alpha_range;
-%if mod((f-1)*timepnts + tp, 8) == 0
 cb = colorbar;
-%    set(cb, 'FontSize', 15);
-%    ylabel(cb, 'Power [dB]', 'FontSize', 15);
-%end
+set(cb, 'FontSize', 15);
+ylabel(cb, 'Power [dB]', 'FontSize', 15);
 
 % Set time window
-startTmpnt = find(gatfr6.time == timepoints(tp));
-endTmpnt = find(gatfr6.time == timepoints(tp+1));
+startTmpnt = find(gatfr6.time == 1);
+endTmpnt = find(gatfr6.time == 2);
 cfg.xlim = [gatfr6.time(startTmpnt) gatfr6.time(endTmpnt)];
 
 % Find max power value for frequency band
 freq_idx = find(gatfr6.freq >= alpha_range(1) & gatfr6.freq <= alpha_range(2));
 timepnts_idx = find(gatfr6.time == 0) : find(gatfr6.time == 2); % Timepoints over entire interval
-submat = gatfr6.powspctrm(1:end-3, freq_idx, timepnts_idx); % [chan x freq x time]
-max_spctrm = max(submat(:), [], 'omitnan');
-max_spctrm = 1.2      %abs(max_spctrm);
+mat.powspctrm = gatfr6.powspctrm(1:end-3, freq_idx, timepnts_idx); % [chan x freq x time]
+max_spctrm = max(mat.powspctrm(:), [], 'omitnan');
+abs(max_spctrm);
 %cfg.zlim = [-max_spctrm max_spctrm];
 
 % Create Topoplot
 ft_topoplotTFR(cfg, gatfr6);
-title(sprintf('%.0f - %.0f ms', 1000*timepoints(tp), 1000*timepoints(tp+1)), 'FontSize', 15);
 
 % Save figure
 saveas(gcf, '/Volumes/methlab/Students/Arne/AOC/figures/eeg/topos/raster/AOC_alpha_power_sternberg_topo6_bl.png');

8d36d64a45fa0531b40cffded5f84556ed0cb19f
Arne Hansen <arne96.hansen@gmail.com>
2025-10-07 15:00:19 +0200

    Update AOC_tfr_sternberg_bl_relchange.m

--- 3_visualization/eeg/tfr/AOC_tfr_sternberg_bl_relchange.m
+++ 3_visualization/eeg/tfr/AOC_tfr_sternberg_bl_relchange.m
@@ -9,9 +9,9 @@ for subj = 1:length(subjects)
     datapath = strcat(path,subjects{subj}, '/eeg');
     cd(datapath)
     load tfr_stern
-    tfr2_all{subj} = tfr2%_bl;
-    tfr4_all{subj} = tfr4%_bl;
-    tfr6_all{subj} = tfr6%_bl;
+    tfr2_all{subj} = tfr2_fooof;
+    tfr4_all{subj} = tfr4_fooof;
+    tfr6_all{subj} = tfr6_fooof;
     disp(['Subject ' num2str(subj) '/' num2str(length(subjects)) ' TFR data loaded.'])
 end
 
@@ -34,7 +34,7 @@ channels = occ_channels;
 
 %% Plot TFR for each individual condition
 close all
-fontSize = 50;
+fontSize = 40;
 
 % Define the common configuration
 cfg = [];
@@ -59,7 +59,7 @@ max_spctrm = max([mean(gatfr2.powspctrm(channel_idx, freq_idx, time_idx), 'omitn
                   mean(gatfr4.powspctrm(channel_idx, freq_idx, time_idx), 'omitnan'); ...
                   mean(gatfr6.powspctrm(channel_idx, freq_idx, time_idx), 'omitnan')]);
 max_spctrm = max(max(abs(max_spctrm)));
-max_spctrm = 0.75
+% max_spctrm = 0.75
 clim = [-max_spctrm, max_spctrm];
 
 % WM load 2
@@ -76,7 +76,7 @@ xlabel('Time [s]');
 ylabel('Frequency [Hz]');
 set(gca, 'FontSize', fontSize);
 title('Sternberg WM load 2 TFR');
-saveas(gcf, '/Volumes/g_psyplafor_methlab$/Students/Arne/AOC/figures/eeg/tfr/AOC_tfr_sternberg_2_bl_relchange.png');
+saveas(gcf, '/Volumes/g_psyplafor_methlab$/Students/Arne/AOC/figures/eeg/tfr/AOC_tfr_sternberg_2_fooof_bl_relchange.png');
 
 % WM load 4
 figure;
@@ -92,7 +92,7 @@ xlabel('Time [s]');
 ylabel('Frequency [Hz]');
 set(gca, 'FontSize', fontSize);
 title('Sternberg WM load 4 TFR');
-saveas(gcf, '/Volumes/g_psyplafor_methlab$/Students/Arne/AOC/figures/eeg/tfr/AOC_tfr_sternberg_4_bl_relchange.png');
+saveas(gcf, '/Volumes/g_psyplafor_methlab$/Students/Arne/AOC/figures/eeg/tfr/AOC_tfr_sternberg_4_fooof_bl_relchange.png');
 
 % WM load 6
 figure;
@@ -108,48 +108,48 @@ xlabel('Time [s]');
 ylabel('Frequency [Hz]');
 set(gca, 'FontSize', fontSize);
 title('Sternberg WM load 6 TFR');
-saveas(gcf, '/Volumes/g_psyplafor_methlab$/Students/Arne/AOC/figures/eeg/tfr/AOC_tfr_sternberg_6_bl_relchange.png');
+saveas(gcf, '/Volumes/g_psyplafor_methlab$/Students/Arne/AOC/figures/eeg/tfr/AOC_tfr_sternberg_6_fooof_bl_relchange.png');
 
 %% Plot the grand averages for the difference between condition 3 and condition 1
-close all
-
-% Plot the difference
-diff = gatfr6;
-diff.powspctrm = gatfr6.powspctrm - gatfr2.powspctrm;
-
-% Define configuration 
-cfg = [];
-cfg.channel = channels; % specify the channels to include
-cfg.colorbar = 'yes'; % include color bar
-cfg.zlim = 'maxabs'; % color limits
-cfg.xlim = [-.5 2]; % Time axis limits in secon
-cfg.ylim = [7 20];
-load('/Volumes/g_psyplafor_methlab$/Students/Arne/MA/headmodel/layANThead.mat'); % Load layout
-cfg.layout = layANThead; % your specific layout
-color_map = flipud(cbrewer('div', 'RdBu', 64)); % 'RdBu' for blue to red diverging color map
-
-% Find maximum deviation
-[~, channel_idx] = ismember(channels, gatfr2.label);
-time_idx = find(diff.time >= -0.5 & diff.time <= 2);
-freq_idx = find(gatfr2.freq >= 8 & gatfr2.freq <= 14);
-max_spctrm = max(abs(diff.powspctrm(channel_idx, freq_idx, time_idx)), [], 'all');
-max_spctrm = .6
-clim = double([-max_spctrm max_spctrm]);
-
-% Plot: Difference Time-Frequency Response
-figure;
-set(gcf, 'Position', [100, 200, 2000, 1200], 'Color', 'w');
-ft_singleplotTFR(cfg, diff);
-colormap(color_map);
-set(gca, 'CLim', clim); 
-cb = colorbar;
-ylabel(cb, 'Power [%]', 'FontSize', fontSize);
-%ylabel(cb, 'Power [\muV^2/Hz]', 'FontSize', fontSize);
-xlabel('Time [s]');
-ylabel('Frequency [Hz]');
-rectangle('Position', [1, 8, 1, 6], 'EdgeColor', 'r', 'LineWidth', 5);
-title('Sternberg TFR Difference (WM load 6 minus WM load 2)', 'FontName', 'Arial', 'FontSize', 30);
-set(gca, 'FontSize', fontSize);
-
-% Save
-saveas(gcf, '/Volumes/g_psyplafor_methlab$/Students/Arne/AOC/figures/eeg/tfr/AOC_tfr_sternberg_diff_bl_relchange.png');
+% close all
+% 
+% % Plot the difference
+% diff = gatfr6;
+% diff.powspctrm = gatfr6.powspctrm - gatfr2.powspctrm;
+% 
+% % Define configuration 
+% cfg = [];
+% cfg.channel = channels; % specify the channels to include
+% cfg.colorbar = 'yes'; % include color bar
+% cfg.zlim = 'maxabs'; % color limits
+% cfg.xlim = [-.5 2]; % Time axis limits in secon
+% cfg.ylim = [7 20];
+% load('/Volumes/g_psyplafor_methlab$/Students/Arne/MA/headmodel/layANThead.mat'); % Load layout
+% cfg.layout = layANThead; % your specific layout
+% color_map = flipud(cbrewer('div', 'RdBu', 64)); % 'RdBu' for blue to red diverging color map
+% 
+% % Find maximum deviation
+% [~, channel_idx] = ismember(channels, gatfr2.label);
+% time_idx = find(diff.time >= -0.5 & diff.time <= 2);
+% freq_idx = find(gatfr2.freq >= 8 & gatfr2.freq <= 14);
+% max_spctrm = max(abs(diff.powspctrm(channel_idx, freq_idx, time_idx)), [], 'all');
+% max_spctrm = .6
+% clim = double([-max_spctrm max_spctrm]);
+% 
+% % Plot: Difference Time-Frequency Response
+% figure;
+% set(gcf, 'Position', [100, 200, 2000, 1200], 'Color', 'w');
+% ft_singleplotTFR(cfg, diff);
+% colormap(color_map);
+% set(gca, 'CLim', clim); 
+% cb = colorbar;
+% ylabel(cb, 'Power [%]', 'FontSize', fontSize);
+% %ylabel(cb, 'Power [\muV^2/Hz]', 'FontSize', fontSize);
+% xlabel('Time [s]');
+% ylabel('Frequency [Hz]');
+% rectangle('Position', [1, 8, 1, 6], 'EdgeColor', 'r', 'LineWidth', 5);
+% title('Sternberg TFR Difference (WM load 6 minus WM load 2)', 'FontName', 'Arial', 'FontSize', 30);
+% set(gca, 'FontSize', fontSize);
+% 
+% % Save
+% saveas(gcf, '/Volumes/g_psyplafor_methlab$/Students/Arne/AOC/figures/eeg/tfr/AOC_tfr_sternberg_diff_fooof_bl_relchange.png');

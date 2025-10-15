892c77566f3e66c8a4ce342f7dd3a77956056949
Arne Hansen <arne96.hansen@gmail.com>
2025-10-15 13:53:38 +0200

    master matrix sternberg trials WORKING

--- 2_feature_extraction/AOC_master_matrix_sternberg_trials.m
+++ 2_feature_extraction/AOC_master_matrix_sternberg_trials.m
@@ -4,82 +4,77 @@
 clear
 clc
 close all
-path = '/Volumes/methlab/Students/Arne/AOC/data/automagic_nohp';
+path = '/Volumes/g_psyplafor_methlab$/Students/Arne/AOC/data/automagic_nohp';
 dirs = dir(path);
 folders = dirs([dirs.isdir] & ~ismember({dirs.name}, {'.', '..'}));
 subjects = {folders.name};
 
 %% Load data
 % Demographics from methlab_vp
-demog_data_sternberg = readtable('/Volumes/methlab_vp/OCC/AOC/AOC_VPs.xlsx');
+demog_data_sternberg = readtable('/Volumes/g_psyplafor_methlab$/VP/OCC/AOC/AOC_VPs.xlsx');
 demog_data_sternberg = demog_data_sternberg(:, {'ID', 'Gender', 'Alter', 'H_ndigkeit', 'OcularDominance'});
 demog_data_sternberg = table2struct(demog_data_sternberg(1:120, :));
 
 % Behavioral
-load('/Volumes/methlab/Students/Arne/AOC/data/features/behavioral_matrix_sternberg.mat');
-
-% EEG
-load('/Volumes/methlab/Students/Arne/AOC/data/features/eeg_matrix_sternberg.mat');
+load('/Volumes/g_psyplafor_methlab$/Students/Arne/AOC/data/features/behavioral_matrix_sternberg_trials.mat');
 
 % Gaze
-load('/Volumes/methlab/Students/Arne/AOC/data/features/gaze_matrix_sternberg.mat');
+load('/Volumes/g_psyplafor_methlab$/Students/Arne/AOC/data/features/gaze_matrix_sternberg_trials.mat');
+gaze_data_sternberg_trials = rmfield(gaze_data_sternberg_trials, 'ScanPathSeriesT');
+gaze_data_sternberg_trials = rmfield(gaze_data_sternberg_trials, 'ScanPathSeries');
 
-
-%% MERGE ON ACTUAL GLOBAL TRIAL IDS
-
-
-safas
-
-% convert structs to tables and inner-join on the canonical key
-T_eeg  = struct2table(subj_data_eeg_trials);
-T_gaze = struct2table(subj_data_gaze_trial);
-
-T_join = innerjoin(T_eeg, T_gaze, ...
-    'Keys', {'ID','GlobalTrial','Condition'}, ...
-    'RightVariables', setdiff(T_gaze.Properties.VariableNames, {'ID','GlobalTrial','Condition','Trial'}));
-
-% final guard
-assert(height(T_join) == height(T_eeg), 'EEG–ET join dropped trials; investigate mismatched keys.');
-
-
-
+% EEG
+load('/Volumes/g_psyplafor_methlab$/Students/Arne/AOC/data/features/eeg_matrix_sternberg_trials.mat');
 
 %% Merge structures
+%  based on global trial IDs
+
+% Add demographic infos to behavioral structure
 demoIDs = [demog_data_sternberg.ID];
-for i = 1:numel(behav_data_sternberg)
-    idx = find(demoIDs == behav_data_sternberg(i).ID, 1);
-    behav_data_sternberg(i).Gender           = demog_data_sternberg(idx).Gender;
-    behav_data_sternberg(i).Alter            = demog_data_sternberg(idx).Alter;
-    behav_data_sternberg(i).H_ndigkeit       = demog_data_sternberg(idx).H_ndigkeit;
-    behav_data_sternberg(i).OcularDominance  = demog_data_sternberg(idx).OcularDominance;
+for i = 1:numel(behav_data_sternberg_trials)
+    idx = find(demoIDs == behav_data_sternberg_trials(i).ID, 1);
+    behav_data_sternberg_trials(i).Gender           = demog_data_sternberg(idx).Gender;
+    behav_data_sternberg_trials(i).Alter            = demog_data_sternberg(idx).Alter;
+    behav_data_sternberg_trials(i).H_ndigkeit       = demog_data_sternberg(idx).H_ndigkeit;
+    behav_data_sternberg_trials(i).OcularDominance  = demog_data_sternberg(idx).OcularDominance;
 end
 
-% Merge all
-merged_data_sternberg = struct( ...
-    'ID', {behav_data_sternberg.ID}, ...
-    'Gender', {behav_data_sternberg.Gender}, ...
-    'Age', {behav_data_sternberg.Alter}, ...
-    'Handedness', {behav_data_sternberg.H_ndigkeit}, ...
-    'OcularDominance', {behav_data_sternberg.OcularDominance}, ...
-    'Condition', {behav_data_sternberg.Condition}, ...
-    'Accuracy', {behav_data_sternberg.Accuracy}, ...
-    'ReactionTime', {behav_data_sternberg.ReactionTime}, ...
-    'GazeDeviation', {gaze_data_sternberg.GazeDeviation}, ...
-    'GazeStdX', {gaze_data_sternberg.GazeStdX}, ...
-    'GazeStdY', {gaze_data_sternberg.GazeStdY}, ...
-    'PupilSize', {gaze_data_sternberg.PupilSize}, ...
-    'MSRate', {gaze_data_sternberg.MSRate}, ...
-    'Blinks', {gaze_data_sternberg.Blinks}, ...
-    'Fixations', {gaze_data_sternberg.Fixations}, ...
-    'Saccades', {gaze_data_sternberg.Saccades}, ...
-    'AlphaPower', {eeg_data_sternberg.AlphaPower}, ...
-    'IAF', {eeg_data_sternberg.IAF},...
-    'Lateralization', {eeg_data_sternberg.Lateralization});
+% convert structs to tables
+T_behav = struct2table(behav_data_sternberg_trials);
+T_gaze = struct2table(gaze_data_sternberg_trials);
+T_eeg  = struct2table(eeg_data_sternberg_trials);
+
+% inner-join on key variables ID, Trial and Condition
+mergeEEGxBehav = innerjoin(T_eeg, T_behav, 'Keys', {'ID','Trial','Condition'});
+merged_data_sternberg_trials = innerjoin(mergeEEGxBehav, T_gaze, 'Keys', {'ID','Trial','Condition'});
+
+%% Re-arrange table
+newOrder = [ ...
+    {'ID', 'Trial', 'Condition'}, ...
+    {'Gender', 'Alter', 'H_ndigkeit', 'OcularDominance'}, ...
+    {'Accuracy', 'ReactionTime', 'Stimuli', 'Probe', 'Match'}, ...
+    {'GazeDeviationEarly', 'GazeDeviationEarlyBL', ...
+    'GazeDeviationLate', 'GazeDeviationLateBL', ...
+    'ScanPathLengthEarly', 'ScanPathLengthEarlyBL', ...
+    'ScanPathLengthLate', 'ScanPathLengthLateBL', ...
+    'PupilSizeEarly', 'PupilSizeEarlyBL', ...
+    'PupilSizeLate', 'PupilSizeLateBL', ...
+    'MSRateEarly', 'MSRateEarlyBL', ...
+    'MSRateLate', 'MSRateLateBL'}, ...
+    {'AlphaPowerEarly', 'AlphaPowerEarlyBL', ...
+    'AlphaPowerLate', 'AlphaPowerLateBL', ...
+    'IAF', 'Lateralization'}];
+
+% Optionally overwrite
+merged_data_sternberg_trials = merged_data_sternberg_trials(:, newOrder)
+
+%% Rename variables
+merged_data_sternberg_trials.Properties.VariableNames{'Alter'} = 'Age';
+merged_data_sternberg_trials.Properties.VariableNames{'H_ndigkeit'} = 'Handedness';
 
 %% Save as .mat
-save /Volumes/methlab/Students/Arne/AOC/data/features/merged_data_sternberg.mat merged_data_sternberg
+save /Volumes/g_psyplafor_methlab$/Students/Arne/AOC/data/features/merged_data_sternberg_trials.mat merged_data_sternberg_trials
 
 %% Save as .csv
-merged_table = struct2table(merged_data_sternberg);
-csv_filename = '/Volumes/methlab/Students/Arne/AOC/data/features/merged_data_sternberg.csv';
-writetable(merged_table, csv_filename);
+csv_filename = '/Volumes/g_psyplafor_methlab$/Students/Arne/AOC/data/features/merged_data_sternberg_trials.csv';
+writetable(merged_data_sternberg_trials, csv_filename);

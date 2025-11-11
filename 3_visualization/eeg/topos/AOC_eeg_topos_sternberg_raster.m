a6d3c1d3285ac95b043afe0634fd4d2d897fc34b
Arne Hansen <arne96.hansen@gmail.com>
2025-11-11 14:27:38 +0100

    Update AOC_eeg_topos_sternberg_raster.m

--- 3_visualization/eeg/topos/AOC_eeg_topos_sternberg_raster.m
+++ 3_visualization/eeg/topos/AOC_eeg_topos_sternberg_raster.m
@@ -49,8 +49,9 @@ timepoints = 0:0.25:2;
 timepnts = length(timepoints)-1;
 nCols = timepnts + 1;
 
-% Plot
-set(gcf, 'Position', [0, 0, 1512, 982], 'Color', 'W');
+% Plot 
+figure;
+set(gcf, 'Position', [0, 0, 1512, 982], 'Color', 'W'); 
 title('Sternberg WM load 6 Theta, Alpha, and Beta Topolots over Time', 'FontSize', 25)
 for f = 1:freqs
     freqband_name = freq_bands{f, 1};
@@ -58,22 +59,17 @@ for f = 1:freqs
 
     % Find max power value for frequency band
     freq_idx = find(gatfr6.freq >= freqband_range(1) & gatfr6.freq <= freqband_range(2));
-    timepnts_idx = find(gatfr6.time == 0) : find(gatfr6.time == 2); % Timepoints over entire interval
+    timepnts_idx = find(gatfr6.time == 0) : find(gatfr6.time == 2);
     dat = gatfr6.powspctrm(1:125, freq_idx, timepnts_idx);
     dat = abs(dat(:));
     max_spctrm = prctile(dat, 95);
 
     for tp = 1:timepnts
-        ax = subplot(freqs, timepnts, (f-1)*timepnts + tp);
-        fig = ax;
-
-        % Set up configuration
+        ax = subplot(freqs, nCols, (f-1)*nCols + tp);
         cfg = [];
         cfg.layout = headmodel.layANThead;
         allchannels = cfg.layout.label;
         cfg.figure = ax;
-
-        %cfg.figure = 'no';
         cfg.channel = allchannels(1:end-2);
         cfg.channel = cfg.channel(~strcmp(cfg.channel, 'M2'));
         cfg.channel = cfg.channel(~strcmp(cfg.channel, 'M1'));
@@ -88,26 +84,28 @@ for f = 1:freqs
         cfg.gridscale = 300;
         cfg.ylim = freqband_range;
         cfg.zlim = [-max_spctrm max_spctrm];
-        %if mod((f-1)*timepnts + tp, 8) == 0
-            cb = colorbar;
-        %    set(cb, 'FontSize', 15);
-        %    ylabel(cb, 'Power [dB]', 'FontSize', 15);
-        %end
-
-        % Set time window
         startTmpnt = find(gatfr6.time == timepoints(tp));
         endTmpnt = find(gatfr6.time == timepoints(tp+1));
         cfg.xlim = [gatfr6.time(startTmpnt) gatfr6.time(endTmpnt)];
 
-        % Create Topoplot
         ft_topoplotTFR(cfg, gatfr6);
-        title(sprintf('%.0f - %.0f ms', 1000*timepoints(tp), 1000*timepoints(tp+1)), 'FontSize', 15);
-
+        title(sprintf('%.0f - %.0f ms', 1000*timepoints(tp), 1000*timepoints(tp+1)), 'FontSize', 12);
     end
+
+    % Colorbar for frequency band
+    ax_cb = subplot(freqs, nCols, f*nCols);
+    set(ax_cb, 'Visible', 'off');
+    cb = colorbar(ax_cb, 'Location', 'eastoutside');
+    colormap(cmap);
+    clim(cfg.zlim);
+    set(cb, 'FontSize', 15);
+    ylabel(cb, 'Power [dB]', 'FontSize', 15);
 end
-text(-12.5, 4.5, "Theta (4-8Hz)", 'FontSize', 20, 'FontWeight', 'bold', 'Rotation', 90);
-text(-12.5, 2, "Alpha (8-14Hz)", 'FontSize', 20, 'FontWeight', 'bold', 'Rotation', 90);
-text(-12.5, -1, "Beta (14-30Hz)", 'FontSize', 20, 'FontWeight', 'bold', 'Rotation', 90);
+
+% Freq bands
+text(-20, 2.85, "Theta (4-8Hz)", 'FontSize', 20, 'FontWeight', 'bold', 'Rotation', 90);
+text(-20, 1.35, "Alpha (8-14Hz)", 'FontSize', 20, 'FontWeight', 'bold', 'Rotation', 90);
+text(-20, -.05, "Beta (14-30Hz)", 'FontSize', 20, 'FontWeight', 'bold', 'Rotation', 90);
 
 % Save figure
 saveas(gcf, '/Volumes/g_psyplafor_methlab$/Students/Arne/AOC/figures/eeg/topos/raster/AOC_alpha_power_sternberg_rasterplot_WM6.png');

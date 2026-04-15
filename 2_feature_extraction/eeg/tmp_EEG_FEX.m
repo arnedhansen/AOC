f06612c3a9ad1159d16269e3c8f3acfc9edf0aa3
Arne Hansen <arne96.hansen@gmail.com>
2026-04-15 16:37:26 +0200

    Update tmp_EEG_FEX.m

--- 2_feature_extraction/eeg/tmp_EEG_FEX.m
+++ 2_feature_extraction/eeg/tmp_EEG_FEX.m
@@ -8,28 +8,27 @@ startup
 
 disp('=== AOC overnight pipeline start ===')
 disp(datestr(now, 31))
-repoRoot = 'C:\Users\Administrator\Documents\GitHub\AOC';
 
 % 1) Non-FOOOF EEG (creates IAF CSV used by TFR FOOOF scripts)
 disp('--- RUN: AOC_eeg_fex_sternberg.m ---')
-run(fullfile(repoRoot, '2_feature_extraction', 'eeg', 'AOC_eeg_fex_sternberg.m'));
+run('C:\Users\Administrator\Documents\GitHub\AOC\2_feature_extraction\eeg\AOC_eeg_fex_sternberg.m');
 
 disp('--- RUN: AOC_eeg_fex_nback.m ---')
-run(fullfile(repoRoot, '2_feature_extraction', 'eeg', 'AOC_eeg_fex_nback.m'));
+run('C:\Users\Administrator\Documents\GitHub\AOC\2_feature_extraction\eeg\AOC_eeg_fex_nback.m');
 
 % 2) TFR FOOOF EEG (reads IAF from non-FOOOF CSV)
 disp('--- RUN: AOC_eeg_fex_sternberg_TFR.m ---')
-run(fullfile(repoRoot, '2_feature_extraction', 'eeg', 'AOC_eeg_fex_sternberg_TFR.m'));
+run('C:\Users\Administrator\Documents\GitHub\AOC\2_feature_extraction\eeg\AOC_eeg_fex_sternberg_TFR.m');
 
 disp('--- RUN: AOC_eeg_fex_nback_TFR.m ---')
-run(fullfile(repoRoot, '2_feature_extraction', 'eeg', 'AOC_eeg_fex_nback_TFR.m'));
+run('C:\Users\Administrator\Documents\GitHub\AOC\2_feature_extraction\eeg\AOC_eeg_fex_nback_TFR.m');
 
 % 3) Master matrices
 disp('--- RUN: AOC_master_matrix_sternberg.m ---')
-run(fullfile(repoRoot, '2_feature_extraction', 'AOC_master_matrix_sternberg.m'));
+run('C:\Users\Administrator\Documents\GitHub\AOC\2_feature_extraction\AOC_master_matrix_sternberg.m');
 
 disp('--- RUN: AOC_master_matrix_nback.m ---')
-run(fullfile(repoRoot, '2_feature_extraction', 'AOC_master_matrix_nback.m'));
+run('C:\Users\Administrator\Documents\GitHub\AOC\2_feature_extraction\AOC_master_matrix_nback.m');
 
 disp('=== AOC overnight pipeline end ===')
 disp(datestr(now, 31))

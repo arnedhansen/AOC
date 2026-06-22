837803f5145486d3c5549cc9dc82b50bf8af4d97
Arne Hansen <arne96.hansen@gmail.com>
2026-06-22 16:11:46 +0200

    ersd electrodes explo

---
+++ 3_visualization/eeg/ersd/AOC_eeg_ersd_nback_ELECTRODES.m
@@ -0,0 +1,162 @@
+%% AOC EEG ERD/ERS N Back time course plus topos
+% Trial-averaged TFR is baselined [-1.5 -0.5] dB per subject, then grand-averaged
+% (matches supervisor occ_alpha_power_script.m).
+
+%% Setup
+startup
+[subjects, paths, colors, headmodel] = setup('AOC');
+path = paths.features;
+figDir = fullfile(paths.figures, 'eeg', 'ersd', 'hardcoded_electrodes');
+if ~isfolder(figDir), mkdir(figDir); end
+subjects = setdiff(subjects, {'361'});
+
+baseline_window = [-1.5 -0.5];
+cfgb = [];
+cfgb.baseline = baseline_window;
+cfgb.baselinetype = 'db';
+
+%% Load trial-averaged TFR, baseline per subject
+tfr1_all = {};
+tfr2_all = {};
+tfr3_all = {};
+tfr1_raw_all = {};
+tfr2_raw_all = {};
+tfr3_raw_all = {};
+
+for subj = 1:numel(subjects)
+    dp = fullfile(path, subjects{subj}, 'eeg');
+    f_tfr = fullfile(dp, 'tfr_nback.mat');
+    T = load(f_tfr, 'tfr1', 'tfr2', 'tfr3');
+    tfr1_raw_all{end + 1} = T.tfr1;
+    tfr2_raw_all{end + 1} = T.tfr2;
+    tfr3_raw_all{end + 1} = T.tfr3;
+    tfr1_all{end + 1} = ft_freqbaseline(cfgb, T.tfr1);
+    tfr2_all{end + 1} = ft_freqbaseline(cfgb, T.tfr2);
+    tfr3_all{end + 1} = ft_freqbaseline(cfgb, T.tfr3);
+    fprintf('Loaded n back TFR %d / %d\n', subj, numel(subjects));
+end
+
+%% HARDCODED ELECTRODES
+chPlot = {'POz', 'PO3', 'PO4', 'PPO5h', 'PPO6h', 'PPO1', 'PPO2', 'Pz', 'P1', 'P2', 'P3', 'P4', 'P5', 'P6'};
+
+%% Grand average (keep individual subjects) and select alpha band
+cfg = [];
+cfg.keepindividual = 'yes';
+ga_nb_1 = ft_freqgrandaverage(cfg, tfr1_all{:});
+ga_nb_2 = ft_freqgrandaverage(cfg, tfr2_all{:});
+ga_nb_3 = ft_freqgrandaverage(cfg, tfr3_all{:});
+
+cfg = [];
+cfg.frequency = [8 14];
+cfg.avgoverfreq = 'yes';
+ga_nb_1_erd = ft_selectdata(cfg, ga_nb_1);
+ga_nb_2_erd = ft_selectdata(cfg, ga_nb_2);
+ga_nb_3_erd = ft_selectdata(cfg, ga_nb_3);
+
+cfg_ga = [];
+cfg_ga.keepindividual = 'yes';
+ga_nb_1_raw = ft_freqgrandaverage(cfg_ga, tfr1_raw_all{:});
+ga_nb_2_raw = ft_freqgrandaverage(cfg_ga, tfr2_raw_all{:});
+ga_nb_3_raw = ft_freqgrandaverage(cfg_ga, tfr3_raw_all{:});
+ga_nb_1_raw_alpha = ft_selectdata(cfg, ga_nb_1_raw);
+ga_nb_2_raw_alpha = ft_selectdata(cfg, ga_nb_2_raw);
+ga_nb_3_raw_alpha = ft_selectdata(cfg, ga_nb_3_raw);
+
+%% Plot ERSD time course (occipital ROI, mean +/- SEM across subjects)
+cfg = [];
+cfg.channel = chPlot;
+cfg.avgoverchan = 'yes';
+cfg.latency = [-.5 2];
+tlk1 = ft_selectdata(cfg, ga_nb_1_erd);
+tlk2 = ft_selectdata(cfg, ga_nb_2_erd);
+tlk3 = ft_selectdata(cfg, ga_nb_3_erd);
+timeVec = tlk1.time(:)';
+
+close all
+figure('Position', [0 0 1512 982], 'Color', 'w');
+fontSize = 30;
+mask = timeVec >= -0.5 & timeVec <= 2;
+x = timeVec(mask);
+
+tc1 = squeeze(tlk1.powspctrm);
+tc2 = squeeze(tlk2.powspctrm);
+tc3 = squeeze(tlk3.powspctrm);
+if isvector(tc1)
+    tc1 = tc1(:)';
+    tc2 = tc2(:)';
+    tc3 = tc3(:)';
+end
+
+y3 = mean(tc3(:, mask), 1, 'omitnan');
+e3 = std(tc3(:, mask), 0, 1, 'omitnan') ./ sqrt(nSubj);
+y2 = mean(tc2(:, mask), 1, 'omitnan');
+e2 = std(tc2(:, mask), 0, 1, 'omitnan') ./ sqrt(nSubj);
+y1 = mean(tc1(:, mask), 1, 'omitnan');
+e1 = std(tc1(:, mask), 0, 1, 'omitnan') ./ sqrt(nSubj);
+
+hold on;
+eb3 = shadedErrorBar(x, y3, e3, 'lineProps', {'-', 'Color', colors(3, :)});
+eb2 = shadedErrorBar(x, y2, e2, 'lineProps', {'-', 'Color', colors(2, :)});
+eb1 = shadedErrorBar(x, y1, e1, 'lineProps', {'-', 'Color', colors(1, :)});
+ebs = [eb3, eb2, eb1];
+cIdx = [3, 2, 1];
+for k = 1:numel(ebs)
+    set(ebs(k).mainLine, 'LineWidth', 2, 'Color', colors(cIdx(k), :));
+    set(ebs(k).patch, 'FaceColor', colors(cIdx(k), :), 'FaceAlpha', 0.20);
+    set(ebs(k).edge(1), 'Color', 'none');
+    set(ebs(k).edge(2), 'Color', 'none');
+end
+xline(0, '--', 'Color', [0.75 0.75 0.75], 'LineWidth', 1);
+yline(0, '--', 'Color', [0.75 0.75 0.75], 'LineWidth', 1);
+
+set(gca, 'FontSize', fontSize);
+xlabel('Time [s]');
+ylabel('Power Change [dB]');
+xlim([-0.5 2]);
+ylim([-3.25 0.65]);
+leg_p3 = patch(NaN, NaN, colors(3, :), 'EdgeColor', 'none');
+leg_p2 = patch(NaN, NaN, colors(2, :), 'EdgeColor', 'none');
+leg_p1 = patch(NaN, NaN, colors(1, :), 'EdgeColor', 'none');
+legend([leg_p1, leg_p2, leg_p3], {'1-back', '2-back', '3-back'}, ...
+    'Location', 'southeast', 'FontSize', 25, 'Box', 'off');
+drawnow; pause(0.05);
+saveas(gcf, fullfile(figDir, 'AOC_eeg_ersd_nback_timecourse_ELECTRODES.png'));
+
+%% Topoplots (retention [0 2] s)
+xlimTopo = [0 2];
+
+cmap = interp1(linspace(0, 1, 5), ...
+    [0.02 0.19 0.58; 0.40 0.67 0.87; 0.97 0.97 0.97; 0.94 0.50 0.36; 0.40 0 0.05], ...
+    linspace(0, 1, 64));
+
+topoData = {ga_nb_1_erd, ga_nb_2_erd, ga_nb_3_erd};
+topoTitles = {'1-back', '2-back', '3-back'};
+
+cfg = [];
+cfg.layout = headmodel.layANThead;
+cfg.zlim = 'maxabs';
+cfg.xlim = xlimTopo;
+cfg.marker = 'off';
+cfg.highlight = 'on';
+cfg.highlightchannel = chPlot;
+cfg.highlightsymbol = '.';
+cfg.highlightsize = 20;
+cfg.comment = 'no';
+cfg.gridscale = 300;
+cfg.colormap = cmap;
+
+figure('Position', [0 0 1512 982*0.6], 'Color', 'w');
+tiledlayout(1, 3, 'TileSpacing', 'compact', 'Padding', 'loose');
+for k = 1:3
+    ax = nexttile(k);
+    cfg.figure = ax;
+    ft_topoplotER(cfg, topoData{k});
+    if k == 3
+        cb = colorbar(ax, 'eastoutside');
+        cb.Label.String = 'Power Change [dB]';
+    end
+    set(ax, 'FontSize', fontSize);
+    title(ax, topoTitles{k}, 'Interpreter', 'none');
+end
+drawnow; pause(0.05);
+saveas(gcf, fullfile(figDir, 'AOC_eeg_ersd_nback_topos_ELECTRODES.png'));
---
+++ 3_visualization/eeg/ersd/AOC_eeg_ersd_sternberg_ELECTRODES.m
@@ -0,0 +1,163 @@
+%% AOC EEG ERD/ERS Sternberg time course plus topos
+% Trial-averaged TFR is baselined [-1.5 -0.5] dB per subject, then grand-averaged
+% (matches supervisor occ_alpha_power_script.m).
+
+%% Setup
+startup
+[subjects, paths, colors, headmodel] = setup('AOC');
+path = paths.features;
+figDir = fullfile(paths.figures, 'eeg', 'ersd', 'hardcoded_electrodes');
+if ~isfolder(figDir), mkdir(figDir); end
+subjects = setdiff(subjects, {'361'});
+
+baseline_window = [-1.5 -0.5];
+cfgb = [];
+cfgb.baseline = baseline_window;
+cfgb.baselinetype = 'db';
+
+%% Load trial-averaged TFR, baseline per subject
+tfr2_all = {};
+tfr4_all = {};
+tfr6_all = {};
+tfr2_raw_all = {};
+tfr4_raw_all = {};
+tfr6_raw_all = {};
+
+for subj = 1:numel(subjects)
+    dp = fullfile(path, subjects{subj}, 'eeg');
+    f_tfr = fullfile(dp, 'tfr_stern.mat');
+    T = load(f_tfr, 'tfr2', 'tfr4', 'tfr6');
+    tfr2_raw_all{end + 1} = T.tfr2;
+    tfr4_raw_all{end + 1} = T.tfr4;
+    tfr6_raw_all{end + 1} = T.tfr6;
+    tfr2_all{end + 1} = ft_freqbaseline(cfgb, T.tfr2);
+    tfr4_all{end + 1} = ft_freqbaseline(cfgb, T.tfr4);
+    tfr6_all{end + 1} = ft_freqbaseline(cfgb, T.tfr6);
+    fprintf('Loaded Sternberg TFR %d / %d\n', subj, numel(subjects));
+end
+
+%% HARDCODED ELECTRODES
+chPlot = {'POz', 'POO3h', 'POO4h', 'PO4', 'PPO1', 'PPO2', 'PPO6h', 'P2', 'P4', 'P6'};
+
+%% Grand average (keep individual subjects) and select alpha band
+cfg = [];
+cfg.keepindividual = 'yes';
+ga_sb_2 = ft_freqgrandaverage(cfg, tfr2_all{:});
+ga_sb_4 = ft_freqgrandaverage(cfg, tfr4_all{:});
+ga_sb_6 = ft_freqgrandaverage(cfg, tfr6_all{:});
+
+cfg = [];
+cfg.frequency = [8 14];
+cfg.avgoverfreq = 'yes';
+ga_sb_2_erd = ft_selectdata(cfg, ga_sb_2);
+ga_sb_4_erd = ft_selectdata(cfg, ga_sb_4);
+ga_sb_6_erd = ft_selectdata(cfg, ga_sb_6);
+
+cfg_ga = [];
+cfg_ga.keepindividual = 'yes';
+ga_sb_2_raw = ft_freqgrandaverage(cfg_ga, tfr2_raw_all{:});
+ga_sb_4_raw = ft_freqgrandaverage(cfg_ga, tfr4_raw_all{:});
+ga_sb_6_raw = ft_freqgrandaverage(cfg_ga, tfr6_raw_all{:});
+ga_sb_2_raw_alpha = ft_selectdata(cfg, ga_sb_2_raw);
+ga_sb_4_raw_alpha = ft_selectdata(cfg, ga_sb_4_raw);
+ga_sb_6_raw_alpha = ft_selectdata(cfg, ga_sb_6_raw);
+
+%% Plot ERSD time course (occipital ROI, mean +/- SEM across subjects)
+cfg = [];
+cfg.channel = chPlot;
+cfg.avgoverchan = 'yes';
+cfg.latency = [-.5 3];
+tlk2 = ft_selectdata(cfg, ga_sb_2_erd);
+tlk4 = ft_selectdata(cfg, ga_sb_4_erd);
+tlk6 = ft_selectdata(cfg, ga_sb_6_erd);
+timeVec = tlk2.time(:)';
+
+close all
+figure('Position', [0 0 1512 982], 'Color', 'w');
+fontSize = 30;
+mask = timeVec >= -.5 & timeVec <= 2;
+x = timeVec(mask);
+
+tc2 = squeeze(tlk2.powspctrm);
+tc4 = squeeze(tlk4.powspctrm);
+tc6 = squeeze(tlk6.powspctrm);
+if isvector(tc2)
+    tc2 = tc2(:)';
+    tc4 = tc4(:)';
+    tc6 = tc6(:)';
+end
+
+nSubj = length(subjects);
+y6 = mean(tc6(:, mask), 1, 'omitnan');
+e6 = std(tc6(:, mask), 0, 1, 'omitnan') ./ sqrt(nSubj);
+y4 = mean(tc4(:, mask), 1, 'omitnan');
+e4 = std(tc4(:, mask), 0, 1, 'omitnan') ./ sqrt(nSubj);
+y2 = mean(tc2(:, mask), 1, 'omitnan');
+e2 = std(tc2(:, mask), 0, 1, 'omitnan') ./ sqrt(nSubj);
+
+hold on;
+eb6 = shadedErrorBar(x, y6, e6, 'lineProps', {'-', 'Color', colors(3, :)});
+eb4 = shadedErrorBar(x, y4, e4, 'lineProps', {'-', 'Color', colors(2, :)});
+eb2 = shadedErrorBar(x, y2, e2, 'lineProps', {'-', 'Color', colors(1, :)});
+ebs = [eb6, eb4, eb2];
+cIdx = [3, 2, 1];
+for k = 1:numel(ebs)
+    set(ebs(k).mainLine, 'LineWidth', 2, 'Color', colors(cIdx(k), :));
+    set(ebs(k).patch, 'FaceColor', colors(cIdx(k), :), 'FaceAlpha', 0.20);
+    set(ebs(k).edge(1), 'Color', 'none');
+    set(ebs(k).edge(2), 'Color', 'none');
+end
+xline(0, '--', 'Color', [0.75 0.75 0.75], 'LineWidth', 1);
+yline(0, '--', 'Color', [0.75 0.75 0.75], 'LineWidth', 1);
+
+set(gca, 'FontSize', fontSize);
+xlabel('Time [s]');
+ylabel('Power Change [dB]');
+xlim([-0.5 2]);
+ylim([-3 1.25]);
+leg_p6 = patch(NaN, NaN, colors(3, :), 'EdgeColor', 'none');
+leg_p4 = patch(NaN, NaN, colors(2, :), 'EdgeColor', 'none');
+leg_p2 = patch(NaN, NaN, colors(1, :), 'EdgeColor', 'none');
+legend([leg_p2, leg_p4, leg_p6], {'WM load 2', 'WM load 4', 'WM load 6'}, ...
+    'Location', 'southeast', 'FontSize', 25, 'Box', 'off');
+drawnow; pause(0.05);
+saveas(gcf, fullfile(figDir, 'AOC_eeg_ersd_sternberg_timecourse_ELECTRODES.png'));
+
+%% Topoplots (late retention [1 2] s)
+xlimTopo = [1 2];
+
+cmap = interp1(linspace(0, 1, 5), ...
+    [0.02 0.19 0.58; 0.40 0.67 0.87; 0.97 0.97 0.97; 0.94 0.50 0.36; 0.40 0 0.05], ...
+    linspace(0, 1, 64));
+
+topoData = {ga_sb_2_erd, ga_sb_4_erd, ga_sb_6_erd};
+topoTitles = {'WM load 2', 'WM load 4', 'WM load 6'};
+
+cfg = [];
+cfg.layout = headmodel.layANThead;
+cfg.zlim = 'maxabs';
+cfg.xlim = xlimTopo;
+cfg.marker = 'off';
+cfg.highlight = 'on';
+cfg.highlightchannel = chPlot;
+cfg.highlightsymbol = '.';
+cfg.highlightsize = 20;
+cfg.comment = 'no';
+cfg.gridscale = 300;
+cfg.colormap = cmap;
+
+figure('Position', [0 0 1512 982*0.6], 'Color', 'w');
+tiledlayout(1, 3, 'TileSpacing', 'compact', 'Padding', 'loose');
+for k = 1:3
+    ax = nexttile(k);
+    cfg.figure = ax;
+    ft_topoplotER(cfg, topoData{k});
+    if k == 3
+        cb = colorbar(ax, 'eastoutside');
+        cb.Label.String = 'Power Change [dB]';
+    end
+    set(ax, 'FontSize', fontSize);
+    title(ax, topoTitles{k}, 'Interpreter', 'none');
+end
+drawnow; pause(0.05);
+saveas(gcf, fullfile(figDir, 'AOC_eeg_ersd_sternberg_topos_ELECTRODES.png'));
